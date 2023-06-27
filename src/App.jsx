53a447cc2fd1342f64cd9581fc7ae88d6f7eef08
EHSAN <93027825+Your-Ehsan@users.noreply.github.com>
2023-06-27 18:52:34 +0500

    Some little changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,20 @@ import About from "./components/about";
 import MainLayout from "./layout/main_layout";
 import { fetchRequiredReq } from "./api";
 import Details from "./components/details";
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebase";
-import { getAnalytics } from "firebase/analytics";
-import { getPerformance } from "firebase/performance";
 // import.meta.env.VITE_API_KEY}&s=${title}`
 // console.log(import.meta.env.VITE_API_KEY);
 function App() {
-  const app = initializeApp(firebaseConfig);
-  getAnalytics(app);
-  getPerformance(app);
   return (
     <>
+      {/* <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Home Page</title>
+        <link rel="canonical" href="http://mysite.com/home" />
+        <meta
+          name="description"
+          content="This is a description of the about page"
+        />
+      </Helmet> */}
       <RouterProvider
         router={createBrowserRouter(
           createRoutesFromElements(
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
+import { firebaseConfig } from "./firebase.js";
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+getPerformance(app);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
